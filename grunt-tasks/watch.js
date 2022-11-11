cf3c75580c35882bcafd9bec4c4b86bff5d1cae5
Colin Tester <focus@colintester.com>
2022-11-11 13:11:48 +0000

    Set up /work sub-directory.

--- grunt-tasks/watch.js
+++ grunt-tasks/watch.js
@@ -4,22 +4,26 @@ module.exports = function(grunt) {
   const tasks = {
     options: { livereload: true, livereloadOnError: false },
 
+    // changes to src/js:
     js: {
       files: `${config.srcPath}js/*.js`,
       tasks: ['browserify:dev']
     },
 
+    // changes to src/sass files:
     sass: {
       files: `${config.srcPath}sass/*.{scss,sass}`,
       tasks: ['sass:dev']
     },
 
+    // whenever page common content (src/html) is updated:
     pageCommonContent: {
       files: `${config.srcPath}html/*.html`,
       tasks: ['replace:insertHTML'],
       options: { livereload: false }
     },
 
+    // changes to html files within /local folder:
     html: {
       files: `${config.devPath}**/*.html`,
     }
