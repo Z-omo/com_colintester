82dcead21aeb5a03023c811b9c10157a868c1169
Colin Tester <focus@colintester.com>
2022-11-24 15:25:03 +0000

    Refine replace code.

--- grunt-tasks/replace.js
+++ grunt-tasks/replace.js
@@ -29,6 +29,7 @@ module.exports = function(grunt) {
   const tasks = {
     options: { overwrite: true },
 
+    // Find page common content placeholders and populate with required content.
     insertHTML: {
       replacements: [{
         from: /(<\!-- insert \"(.*)\" -->)[\s\S]*?(<\!-- end insert -->)/ig,
@@ -36,31 +37,41 @@ module.exports = function(grunt) {
       }],
   
       overwrite: true,
-
       src: [`${config.devPath}**/*.html`]
     },
 
+    /*
+     * Finds and converts local path links to URI without the file extension.
+     *
+     * Found matches are passed to our custom ‘to:’ function.
+     * @param {String} attr – representing found full match.
+     * @param {Number} i – index point where mtach was found in full text.
+     * @param {String} ft – source full text.
+     * @param {Array} matches – captured references.
+     */
     localLinks: {
       replacements: [{
         from: /href=\"(.+?)\.html\"/ig,
         to: (attr, i, ft, matches) => {
-          // assign first path component:
+          // assign first component from the matched path:
           const path = matches[0].split('/')[0];
           
           /*
-           * return the full found href attribute, if found path is not
-           * one of the expected local paths:
+           * if found path is not one of the expected local paths
+           * return the full found href attribute:
            */
           if (-1 === localPaths.indexOf(path)) { return attr; }
 
-          // otherwise, set href attribute value without the file extension.
+          /*
+           * otherwise, set href attribute value without the file extension.
+           * Note: we substitute a relative link for ‘index’ path.
+           */
           const p = ('index' === path) ? './' : matches[0];
           return `href="${p}"`;
         }
       }],
   
       overwrite: true,
-
       src: [`${config.buildPath}**/*.html`]
     }
   };
